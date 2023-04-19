ffbcede4fd669fb7b1c3edbd33aa788b589c1b61
ChristosPetrou <c.petrou@tiomarkets.com>
2023-04-19 15:45:23 +0300

    fixing cors

--- sanity.cli.js
+++ sanity.cli.js
@@ -3,6 +3,9 @@ import {defineCliConfig} from 'sanity/cli'
 export default defineCliConfig({
   api: {
     projectId: 'n5l86icy',
-    dataset: 'production'
-  }
+    dataset: 'production',
+    cors: {
+      origin: ['https://personal-blog-chrpetrou.vercel.app'],
+    },
+  },
 })
